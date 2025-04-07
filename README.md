295847881509dc5bb6ae6a2dd37676f897024d5b
kairsen <kairsen.lau@gmail.com>
2025-04-07 16:30:13 +0800

    Update files

--- README.md
+++ README.md
@@ -1,11 +1,6 @@
 云编译7981固件
 （p3terx 2024新版 基于ubuntu22.04）
 
-360T7主路由固件，含插件DDNS, PASSWALL（用最新源码编译）。
-
-JCG Q30 PRO极简AP固件。
-
-
 237大佬源码网址:
 [https://github.com/padavanonly/immortalwrt-mt798x-24.10](https://github.com/padavanonly/immortalwrt-mt798x-24.10)
 
@@ -14,5 +9,4 @@ JCG Q30 PRO极简AP固件。
 
 ----------------------------------------------------------------
 注意，新建的repositories ，要修改权限（原来只读，改成读写权限）。
-
 Settings，Actions，General，右侧栏拉到最下面，	Workflow permissions，勾选 Read and write permissions
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,23 +1,100 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 移除 openwrt feeds 自带的核心包
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 更新 golang 1.23 版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 替换 Passwall 软件
+rm -rf feeds/luci/applications/luci-app-passwall/*
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
+mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+rm -rf passwall
 
-#rm -rf feeds/packages/utils/watchcat
-#git clone --depth=1 https://github.com/openwrt/packages/tree/master/utils/watchcat feeds/packages/utils/watchcat
+# 添加 Passwall2 软件
+# rm -rf feeds/luci/applications/luci-app-passwall2
+mkdir feeds/luci/applications/luci-app-passwall2
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall2 passwall2
+mv passwall2/luci-app-passwall2/* feeds/luci/applications/luci-app-passwall2/
+rm -rf passwall2
+
+# 修改 Passwall 检测规则
+sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall/root/usr/share/passwall/app.sh
+
+# 修改 Passwall2 检测规则
+sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall2/root/usr/share/passwall2/app.sh
+
+# 添加 OpenClash 软件
+git clone --depth 1 https://github.com/vernesong/openclash.git OpenClash
+rm -rf feeds/luci/applications/luci-app-openclash
+mv OpenClash/luci-app-openclash feeds/luci/applications/luci-app-openclash
+
+# Remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rfv feeds/packages/net/v2ray-geodata
+git clone https://github.com/Ljzkirito/v2ray-geodata feeds/packages/net/v2ray-geodata
+rm -rfv feeds/packages/net/mosdns
+find ./ | grep Makefile | grep luci-app-mosdns | xargs rm -fv
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+
+# 替换 Smartdns
+./scripts/feeds uninstall luci-app-smartdns smartdns
+./scripts/feeds install -a -p customsd
+
+# 替换 luci-app-ssr-plus & Depends
+Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client luci-app-ssr-plus"
+./scripts/feeds uninstall ${Replace_package}
+./scripts/feeds install -f -p helloworld ${Replace_package}
+
+# 替换 shadowsocks-rust
+rm -rfv feeds/packages/net/shadowsocks-rust
+git clone https://github.com/Ljzkirito/shadowsocks-rust feeds/packages/net/shadowsocks-rust
+
+# 替换 adguardhome
+rm -rfv feeds/packages/net/adguardhome
+git clone https://github.com/Ljzkirito/adguardhome-openwrt feeds/packages/net/adguardhome
+
+# 修改 Hostname
+sed -i "s/hostname='.*'/hostname='WR30U'/g" package/base-files/files/bin/config_generate
+
+# 修改登录 IP
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+
+# 修改默认 SSID
+sed -i "s/ImmortalWrt-2.4G/Breakwa11/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i "s/ImmortalWrt-5G/Breakwa11 ax/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+
+# ttyd 终端自动登录
+sed -i "s?/bin/login?/usr/libexec/login.sh?g" feeds/packages/utils/ttyd/files/ttyd.config
+
+#git clone --depth=1 -b master https://github.com/fw876/helloworld
+#Replace_package="xray-core xray-plugin v2ray-core v2ray-plugin hysteria ipt2socks microsocks redsocks2 chinadns-ng dns2socks dns2tcp naiveproxy simple-obfs tcping tuic-client"
+#for a in ${Replace_package}
+#do
+#	echo "Replace_package=$a"
+# 	rm -rfv feeds/packages/net/"$a"
+#	cp -rv helloworld/"$a" feeds/packages/net
+#done
+# sed -i 's/ +libopenssl-legacy//g' feeds/packages/net/shadowsocksr-libev/Makefile
+#rm -rfv feeds/luci/applications/luci-app-ssr-plus
+#cp -rv helloworld/luci-app-ssr-plus feeds/luci/applications
+#cp -rv helloworld/shadow-tls package
+#rm -rfv helloworld
+# Remove upx commands
+#makefile_file="$({ find package|grep Makefile |sed "/Makefile./d"; } 2>"/dev/null")"
+#for a in ${makefile_file}
+#do
+#	[ -n "$(grep "upx" "$a")" ] && sed -i "/upx/d" "$a"
+#done
