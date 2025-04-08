0ba4898bdb8f39f055c5df9ad700a1fc56aae9bc
kairsen <kairsen.lau@gmail.com>
2025-04-08 15:19:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,33 +1,19 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
 
-# Replace /lib/netifd/ppp-down
-# https://github.com/hanwckf/immortalwrt-mt798x/issues/57
-cat >> package/network/services/ppp/files/lib/netifd/ppp-down <<EOF
-while true; do
-  if ubus -S list "network.interface.wan_6"; then
-    # ubus call network.interface.wan_6 down
-    logger "waiting wan_6 down..."
-    sleep 1
-  else
-    break
-  fi
-done
-EOF
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 取消 Dnsmasq 缓存，由 smartdns 负责。不修改 ttl
-sed -i 's/cachesize\t8000/cachesize\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-sed -i 's/mini_ttl\t3600/mini_ttl\t0/g' package/network/services/dnsmasq/files/dhcp.conf
-
-# Remove 6in4
-sed -i 's/ +6in4//g' package/emortal/ipv6-helper/Makefile
-sed -i '/hotplug.d/d' package/emortal/ipv6-helper/Makefile
-rm -fv package/emortal/ipv6-helper/files/60-6in4
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
